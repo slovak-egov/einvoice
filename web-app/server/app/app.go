f1bc29c16bdd9bd52834b5c89b0c1b0ddf601989
Samuel Sládek <sladek.samuel@gmail.com>
2020-12-07 18:17:34 +0100

    Graceful shutdown (#14)

--- web-app/server/app/app.go
+++ web-app/server/app/app.go
@@ -3,7 +3,6 @@ package app
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
@@ -40,11 +39,17 @@ func (a *App) Run() {
 	srv := &http.Server{
 		Handler:      a.router,
 		Addr:         fmt.Sprintf("%s:%d", "0.0.0.0", a.config.Port),
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
+		WriteTimeout: a.config.ServerWriteTimeout,
+		ReadTimeout:  a.config.ServerReadTimeout,
 	}
 
 	log.WithField("address", srv.Addr).Info("webapp.server.start")
 
-	log.WithField("error", srv.ListenAndServe()).Fatal("webapp.server.failed")
+	go func() {
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.WithField("error", err.Error()).Fatal("webapp.server.failed")
+		}
+	}()
+
+	handlerutil.GracefulShutdown(srv, a.config.GracefulTimeout)
 }
