585791909eb79b74cb3dd2253f18988785ce5342
Filip Matusak <filip.matusak@gmail.com>
2020-12-11 13:24:55 +0100

    Api key integer 'sub'

--- apiserver/app/apiKey.go
+++ apiserver/app/apiKey.go
@@ -2,7 +2,9 @@ package app
 
 import (
 	goContext "context"
-	"strconv"
+	"errors"
+	"fmt"
+	"math"
 
 	"github.com/dgrijalva/jwt-go"
 	log "github.com/sirupsen/logrus"
@@ -11,6 +13,22 @@ import (
 	"github.com/slovak-egov/einvoice/pkg/context"
 	"github.com/slovak-egov/einvoice/pkg/handlerutil"
 )
+
+func getIntClaim(claims jwt.MapClaims, key string) (int, error) {
+	rawValue, ok := claims[key]
+	if !ok {
+		return 0, errors.New(fmt.Sprintf("Key '%v' not found in claims", key))
+	}
+	switch v := rawValue.(type) {
+	case float64:
+		if math.Mod(v, 1.0) == 0 {
+			return int(v), nil
+		}
+	case int:
+		return v, nil
+	}
+	return 0, errors.New(fmt.Sprintf("Key '%v' in claims has wrong type", key))
+}
 
 func (a *App) getUserIdByApiKey(ctx goContext.Context, tokenString string) (int, error) {
 	var user *entity.User
@@ -25,15 +43,12 @@ func (a *App) getUserIdByApiKey(ctx goContext.Context, tokenString string) (int,
 		}
 
 		var (
-			err error
-			rawUserId string
+			err    error
 			userId int
 		)
-		if rawUserId, ok = claims["sub"].(string); !ok {
-			return nil, handlerutil.NewNotFoundError("User not found")
-		}
-		if userId, err = strconv.Atoi(rawUserId); err != nil {
-			return nil, handlerutil.NewNotFoundError("User not found")
+
+		if userId, err = getIntClaim(claims, "sub"); err != nil {
+			return nil, handlerutil.NewAuthorizationError(err.Error())
 		}
 
 		user, err = a.db.GetUser(ctx, userId)
