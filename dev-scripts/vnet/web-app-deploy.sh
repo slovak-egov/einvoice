2bf8b2ae2903e0e366f6711822fab3e0838ead29
samo98 <sladek.samuel@gmail.com>
2020-12-14 13:14:13 +0100

    Fail redeploy script on any error

--- dev-scripts/vnet/web-app-deploy.sh
+++ dev-scripts/vnet/web-app-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 docker build -t samo98/einvoice-web-app -f Dockerfile-web-app .
 docker push samo98/einvoice-web-app
-$JELASTIC_HOME/environment/control/redeploycontainers --envName einvoice-dev --nodeGroup cp --tag latest
+"$JELASTIC_HOME"/environment/control/redeploycontainers --envName einvoice-dev --nodeGroup cp --tag latest
