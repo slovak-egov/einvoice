ef6ca1355f6867dbf190ba4dfa0fbdcd1bc334a3
Filip Matusak <filip.matusak@gmail.com>
2021-01-14 12:19:04 +0100

    Test invoice expiration

--- dev-scripts/vnet/notificationworker-deploy.sh
+++ dev-scripts/vnet/notificationworker-deploy.sh
@@ -4,4 +4,4 @@ set -e
 
 docker build -t samo98/einvoice-notificationworker -f Dockerfile-notificationworker .
 docker push samo98/einvoice-notificationworker
-"$JELASTIC_HOME"/environment/control/redeploycontainers --envName dev-einvoice-api-mfsr --nodeGroup docker --tag latest
+"$JELASTIC_HOME"/environment/control/redeploycontainers --envName dev-einvoice-api-mfsr --nodeGroup docker2 --tag latest
