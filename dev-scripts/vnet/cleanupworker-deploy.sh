b5c790aac8583c17f1d11d5ef19901141b85eb6d
Filip Matusak <filip.matusak@gmail.com>
2021-01-14 09:23:25 +0100

    Test invoice expiration

--- dev-scripts/vnet/cleanupworker-deploy.sh
+++ dev-scripts/vnet/cleanupworker-deploy.sh
@@ -4,4 +4,4 @@ set -e
 
 docker build -t samo98/einvoice-cleanupworker -f Dockerfile-cleanupworker .
 docker push samo98/einvoice-cleanupworker
-"$JELASTIC_HOME"/environment/control/redeploycontainers --envName einvoice-apiserver-cluster --nodeGroup docker --tag latest
+"$JELASTIC_HOME"/environment/control/redeploycontainers --envName dev-einvoice-api-mfsr --nodeGroup docker --tag latest
