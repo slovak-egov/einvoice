42f0b4d3a03cb1a3486eacbe49ae0ec5435441c6
Filip Matusak <filip.matusak@gmail.com>
2021-01-29 20:04:39 +0100

    PDF generator (#53)
    PDF generator

--- internal/apiserver/visualization/pdf.go
+++ internal/apiserver/visualization/pdf.go
@@ -2,36 +2,101 @@ package visualization
 
 import (
 	"fmt"
-	"strconv"
+	"strings"
 
 	"github.com/jung-kurt/gofpdf"
-
-	"github.com/slovak-egov/einvoice/internal/entity"
+	"github.com/lestrrat-go/libxml2"
+	"github.com/lestrrat-go/libxml2/clib"
+	"github.com/lestrrat-go/libxml2/types"
 )
 
-func Generate(inv *entity.Invoice) *File {
+const (
+	lineHeight = 5
+	tabSize    = 4
+	font       = "Arial"
+)
+
+var pageHeight float64
+
+func generateLines(currentNode types.Node, level int, pdf *gofpdf.Fpdf) error {
+	if currentNode.NodeType() == clib.ElementNode {
+		if pdf.GetX() > pageHeight {
+			pdf.AddPage()
+		}
+
+		// Write padding
+		for i := 0; i < tabSize*(level-1); i++ {
+			pdf.Write(lineHeight, " ")
+		}
+
+		// Write node name in bold red
+		nodeNameParts := strings.Split(currentNode.NodeName(), ":")
+		name := nodeNameParts[len(nodeNameParts)-1]
+		pdf.SetTextColor(186, 24, 24)
+		pdf.SetFontStyle("B")
+		pdf.Write(lineHeight, name)
+
+		// Reset font and text color for writing tag value
+		pdf.SetFontStyle("")
+		pdf.SetTextColor(0, 0, 0)
+
+		// Write value
+		// Check if node contains value and write it
+		if child, err := currentNode.FirstChild(); err == nil && child.NodeType() == clib.TextNode {
+			pdf.Write(lineHeight, ": "+strings.TrimSpace(child.TextContent()))
+		}
+
+		// Write tag attributes in yellow
+		pdf.SetTextColor(191, 143, 31)
+		if e, ok := currentNode.(types.Element); ok {
+			attrs, err := e.Attributes()
+			if err != nil {
+				return err
+			}
+			for _, attr := range attrs {
+				// Skip xsi attributes
+				if strings.HasPrefix(attr.NodeName(), "xsi:") {
+					continue
+				}
+				pdf.Write(lineHeight, fmt.Sprintf(" (%s=%s)", attr.NodeName(), attr.TextContent()))
+			}
+		}
+		pdf.SetTextColor(0, 0, 0)
+
+		// Break line
+		pdf.Write(lineHeight, "\n")
+	}
+
+	children, err := currentNode.ChildNodes()
+	if err != nil {
+		return err
+	}
+	for _, child := range children {
+		if err = generateLines(child, level+1, pdf); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func GeneratePdf(invoiceBytes []byte) (*File, error) {
+	xml, err := libxml2.Parse(invoiceBytes)
+	if err != nil {
+		return nil, err
+	}
+
 	pdf := gofpdf.New("P", "mm", "A4", "")
+	pdf.SetFont(font, "", lineHeight)
+	pdf.SetFontUnitSize(lineHeight)
+	_, pageHeight = pdf.GetPageSize()
 
 	pdf.AddPage()
 
-	pdf.SetFont("Arial", "B", 30)
-	pdf.Text(30, 30, "Invoice "+strconv.Itoa(inv.Id))
+	err = generateLines(xml, 0, pdf)
+	if err != nil {
+		return nil, err
+	}
 
-	pdf.SetFont("Arial", "B", 16)
-
-	pdf.SetFont("Arial", "B", 16)
-	pdf.Text(30, 70, "Supplier")
-	pdf.SetFont("Arial", "B", 10)
-	pdf.Text(30, 80, "Name: "+inv.Sender)
-	pdf.Text(30, 85, "ICO: "+inv.SupplierIco)
-
-	pdf.SetFont("Arial", "B", 16)
-	pdf.Text(110, 70, "Customer")
-	pdf.SetFont("Arial", "B", 10)
-	pdf.Text(110, 80, "Name: "+inv.Receiver)
-	pdf.Text(110, 85, "ICO: "+inv.CustomerIco)
-
-	pdf.Text(30, 110, fmt.Sprintf("Price: %v", inv.Price))
-
-	return &File{pdf}
+	return &File{pdf}, nil
 }
