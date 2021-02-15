92260d288af33c235ec9349ebc3812b52967ad4e
samo98 <sladek.samuel@gmail.com>
2021-02-15 18:40:08 +0100

    Move dockerfiles

--- dev-scripts/jelastic/deploy.sh
+++ dev-scripts/jelastic/deploy.sh
@@ -2,38 +2,41 @@
 
 set -e
 
+# Move to project root directory
+cd "$(dirname "$0")"/../..
+
 case "$1" in
   apiserver)
     DOCKER_NAME="samo98/einvoice-apiserver"
-    DOCKER_FILE="Dockerfile-apiserver"
+    DOCKER_FILE="cmd/apiserver/Dockerfile"
     ENV_NAME="dev-einvoice-api-mfsr"
     NODE_GROUP="cp"
     ;;
 
   web-app)
     DOCKER_NAME="samo98/einvoice-web-app"
-    DOCKER_FILE="Dockerfile-web-app"
+    DOCKER_FILE="cmd/webserver/Dockerfile"
     ENV_NAME="dev-einvoice-mfsr"
     NODE_GROUP="cp"
     ;;
 
   notification-worker)
     DOCKER_NAME="samo98/einvoice-notificationworker"
-    DOCKER_FILE="Dockerfile-notificationworker"
+    DOCKER_FILE="cmd/notification-worker/Dockerfile"
     ENV_NAME="dev-einvoice-api-mfsr"
     NODE_GROUP="docker"
     ;;
 
   cleanup-worker)
     DOCKER_NAME="samo98/einvoice-cleanupworker"
-    DOCKER_FILE="Dockerfile-cleanupworker"
+    DOCKER_FILE="cmd/cleanup-worker/Dockerfile"
     ENV_NAME="dev-einvoice-api-mfsr"
     NODE_GROUP="docker2"
     ;;
 
   invoice-validator)
     DOCKER_NAME="samo98/einvoice-invoice-validator"
-    DOCKER_FILE="Dockerfile-invoice-validator"
+    DOCKER_FILE="invoice-validator/Dockerfile"
     ENV_NAME="dev-einvoice-validator"
     NODE_GROUP="cp"
     ;;
