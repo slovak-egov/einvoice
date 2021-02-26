13c88bfac66578dd78edf2da6d52d7498936e40a
Filip Matusak <filip.matusak@gmail.com>
2021-02-26 08:31:48 +0100

    Rules documentation generator (#85)
    * Rules documentation generator

--- invoice-validator/data/generate-schemas.sh
+++ invoice-validator/data/generate-schemas.sh
@@ -9,3 +9,7 @@ mvn -f pom-xslt.xml process-resources
 npx xslt3 -xsl:ubl2.1/xslt/EN16931-UBL-validation-preprocessed.xslt -export:ubl2.1/en16931-schema.sef.json -t -nogo
 
 npx xslt3 -xsl:d16b/xslt/EN16931-CII-validation-preprocessed.xslt -export:d16b/en16931-schema.sef.json -t -nogo
+
+SCHEMATRON_FILE=ubl2.1/schematron/preprocessed/EN16931-UBL-validation-preprocessed.sch OUTPUT=../../data/schemas/ubl2.1/rules-documentation.json go run rulesDocumentation/main.go
+
+SCHEMATRON_FILE=d16b/schematron/preprocessed/EN16931-CII-validation-preprocessed.sch OUTPUT=../../data/schemas/d16b/rules-documentation.json go run rulesDocumentation/main.go
