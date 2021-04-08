a1f4453e6d5ba5a294cae4cc7b541d6106b86faa
Filip Matusak <filip.matusak@gmail.com>
2021-04-08 13:27:40 +0200

    Create rules - business terms cross links scripts

---
+++ dev-scripts/docs/addBusinessTermsToRules.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+const regex = /(BG|BT)[-0-9]*/g;
+
+let rules = require('./../../data/schemas/ubl2.1/rules-documentation.json');
+
+Object.values(rules).forEach(rule => {
+  const businessTerms = rule.message.en.match(regex);
+  if (businessTerms) {
+    rule.businessTerms = [...new Set(businessTerms)];
+  }
+});
+
+fs.writeFile('./data/schemas/ubl2.1/rules-documentation.json', JSON.stringify(rules), function (err) {
+  if (err) throw err;
+  console.log('saved');
+});
