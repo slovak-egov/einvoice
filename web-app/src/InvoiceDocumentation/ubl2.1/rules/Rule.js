ad4d12fc235b9286b84ae202f8708341a22a58be
Filip Matusak <filip.matusak@gmail.com>
2021-04-08 16:21:13 +0200

    Create rules -> tags cross links

--- web-app/src/InvoiceDocumentation/ubl2.1/rules/Rule.js
+++ web-app/src/InvoiceDocumentation/ubl2.1/rules/Rule.js
@@ -1,5 +1,6 @@
 import {useSelector} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {Fragment} from 'react'
 import {useTranslation} from 'react-i18next'
 import NotFound from '../../../helpers/NotFound'
 import {ubl21RuleSelector} from '../../../cache/documentation/state'
@@ -43,6 +44,38 @@ export default ({match}) => {
         <strong className="govuk-grid-column-one-quarter">{t('invoiceDocs.rules.flag')}</strong>
         <div className="govuk-grid-column-three-quarters">{t(`invoiceDocs.rules.flags.${rule.flag}`)}</div>
       </div>
+      {rule.invoiceTags && <div className="govuk-grid-row">
+        <strong className="govuk-grid-column-one-quarter">{t('invoiceDocs.ublInvoice')}</strong>
+        <div className="govuk-grid-column-three-quarters">
+          {rule.invoiceTags.map((path, i) => (
+            <Fragment key={i}>
+              {i !== 0 && <br />}
+              {path.map((tag, j) => (
+                <Fragment key={j}>
+                  <span> / </span>
+                  <Link to={`/invoiceDocumentation/ublInvoice/${path.slice(0, j + 1).join('/')}`}>{tag}</Link>
+                </Fragment>
+              ))}
+            </Fragment>
+          ))}
+        </div>
+      </div>}
+      {rule.creditNoteTags && <div className="govuk-grid-row">
+        <strong className="govuk-grid-column-one-quarter">{t('invoiceDocs.ublCreditNote')}</strong>
+        <div className="govuk-grid-column-three-quarters">
+          {rule.creditNoteTags.map((path, i) => (
+            <Fragment key={i}>
+              {i !== 0 && <br />}
+              {path.map((tag, j) => (
+                <Fragment key={j}>
+                  <span> / </span>
+                  <Link to={`/invoiceDocumentation/ublCreditNote/${path.slice(0, j + 1).join('/')}`}>{tag}</Link>
+                </Fragment>
+              ))}
+            </Fragment>
+          ))}
+        </div>
+      </div>}
     </>
   )
 }
